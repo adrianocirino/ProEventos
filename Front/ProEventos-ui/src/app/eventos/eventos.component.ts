3d775b1694dd4c6495e3c5bf113c2498e52dbbdb
adrianocirino <adriano.cirino.da.silva@gmail.com>
2021-02-25 00:25:35 -0300

    adicionado filtro por local e tema

--- Front/ProEventos-ui/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-ui/src/app/eventos/eventos.component.ts
@@ -8,7 +8,22 @@ import { EventosService } from '../services/eventos.service';
 })
 export class EventosComponent implements OnInit {
 
-  eventos: any;
+  public eventos: any = [];
+
+  widthImg = 80;
+  marginImg = 2;
+  showImg = true;
+  private filtroListaa = '';
+  public eventosFiltrados: any = [];
+
+  public get filtroLista(): string {
+    return this.filtroListaa;
+  }
+
+  public set filtroLista(value: string) {
+    this.filtroListaa = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
 
   constructor(private service: EventosService) { }
 
@@ -18,7 +33,23 @@ export class EventosComponent implements OnInit {
 
   carregarEventos(): void {
     this.service.getEventos().subscribe(
-      response => this.eventos = response
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
+      error => console.log(error)
+    );
+  }
+
+  ocultarImg(): void {
+    this.showImg = !this.showImg;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+                       evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
     );
   }
 
