bcd21227be02966e2d5b7c7c6ea0af8691e1d68f
adrianocirino <adriano.cirino.da.silva@gmail.com>
2021-03-03 23:14:47 -0300

    Ajustes titulo

--- Front/ProEventos-ui/src/app/shared/titulo/titulo.component.ts
+++ Front/ProEventos-ui/src/app/shared/titulo/titulo.component.ts
@@ -8,6 +8,9 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TituloComponent implements OnInit {
 
   @Input() titulo = '';
+  @Input() iconClass = 'fa fa-user';
+  @Input() subtitulo = 'Desde 2021';
+  @Input() btnListar = false;
 
   constructor() { }
 
