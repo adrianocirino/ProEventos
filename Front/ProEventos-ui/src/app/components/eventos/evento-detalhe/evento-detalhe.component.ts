e56df1c6d06703f787e869ba3b51e1625f4cfe6f
adrianocirino <adriano.cirino.da.silva@gmail.com>
2021-03-04 22:14:24 -0300

    ReactiveForms

--- Front/ProEventos-ui/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Front/ProEventos-ui/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-evento-detalhe',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventoDetalheComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  public validation(): void {
+    this.form = this.formBuilder.group({
+      tema: [null, [ Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+      local: [null, Validators.required],
+      dataEvento: [null, Validators.required],
+      imagemURL: [null, Validators.required],
+      qtdPessoa: [null, [ Validators.required, Validators.maxLength(120000)]],
+      telefone: [null, Validators.required],
+      email: [null, [ Validators.required, Validators.email ]],
+    });
   }
 
 }
