a6b963e95ccd7817de3d16df899ebd6e0b8966c4
adrianocirino <adriano.cirino.da.silva@gmail.com>
2021-03-04 22:32:25 -0300

    Ajuste form

--- Front/ProEventos-ui/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Front/ProEventos-ui/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -8,7 +8,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class EventoDetalheComponent implements OnInit {
 
-  form!: FormGroup;
+  form = this.formBuilder.group({
+    tema: [null, [ Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+    local: [null, Validators.required],
+    dataEvento: [null, Validators.required],
+    imagemURL: [null, Validators.required],
+    qtdPessoa: [null, [ Validators.required, Validators.maxLength(120000)]],
+    telefone: [null, Validators.required],
+    email: [null, [ Validators.required, Validators.email ]],
+  });
 
   get f(): any {
     return this.form.controls;
@@ -17,18 +25,6 @@ export class EventoDetalheComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-  }
-
-  public validation(): void {
-    this.form = this.formBuilder.group({
-      tema: [null, [ Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
-      local: [null, Validators.required],
-      dataEvento: [null, Validators.required],
-      imagemURL: [null, Validators.required],
-      qtdPessoa: [null, [ Validators.required, Validators.maxLength(120000)]],
-      telefone: [null, Validators.required],
-      email: [null, [ Validators.required, Validators.email ]],
-    });
   }
 
 }
