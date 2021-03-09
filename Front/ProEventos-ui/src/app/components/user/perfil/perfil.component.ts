c868845057cc80994baec054f50fd37d6ad6150a
adrianocirino <adriano.cirino.da.silva@gmail.com>
2021-03-08 22:35:47 -0300

    Criado form de perfil

--- Front/ProEventos-ui/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-ui/src/app/components/user/perfil/perfil.component.ts
@@ -1,3 +1,5 @@
+import { ValidatorField } from './../../../helpers/ValidatorField';
+import { FormGroup, FormBuilder, Validators, AbstractControlOptions } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  get f(): any { return this.form.controls; }
+
+  constructor(public formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.validation();
+  }
+
+  private validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmeSenha')
+    };
+
+    this.form = this.formBuilder.group({
+      titulo: [null, [ Validators.required]],
+      primeiroNome: [null, [ Validators.required]],
+      ultimoNome: [null, [ Validators.required]],
+      email: [null, [ Validators.required, Validators.email]],
+      telefone: [null],
+      funcao: [null, Validators.required],
+      descricao: [null],
+      senha: [null, [ Validators.required , Validators.minLength(6)]],
+      confirmeSenha: [null, [ Validators.required]],
+    }, formOptions);
   }
 
 }
